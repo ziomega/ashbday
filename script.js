3da43fb5922ed5cac5764b3e96107f57c65bfd52
Evaan Antony Philip <146336282+ziomega@users.noreply.github.com>
2023-10-14 09:49:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var audioElement = document.getElementById("audio-element");
+
+audioElement.src = "collide.mp3";
+audioElement.type = "audio/mpeg"; 
+ 
+audioElement.autoplay = true;
+audioElement.loop = true;
+
